04783c40861cc98264eceae46e451cbbc7862534
shines77 <gz_shines@msn.com>
2018-06-14 08:48:56 +0800

    添加了一个头文件;
    Signed-off-by: shines77 <gz_shines@msn.com>

---
+++ tech/string-matching/unused_codes/AlgorithmWrapper.h
@@ -0,0 +1,35 @@
+
+namespace StringMatch {
+
+class Pattern {
+
+    /*
+    // Pattern::args
+    tuple_type & get_args() const {
+        return const_cast<tuple_type &>(this->algorithm_.get_args());
+    }
+
+    const tuple_type & get_cargs() const {
+        return this->algorithm_.get_args();
+    }
+
+    void set_args(const tuple_type & args) {
+        this->algorithm_.set_args(args);
+    }
+
+    template <size_t N>
+    inline auto get(const tuple_type & args) const
+        -> decltype(typename std::tuple_element<N, tuple_type>::type) {
+        return std::get<N>(args);
+    }
+
+    template <size_t N>
+    inline void set(const tuple_type & args,
+                    typename std::tuple_element<N, tuple_type>::type && value) const {
+        std::set<N>(args, value);
+    }
+    */
+
+};
+
+} // namespace StringMatch
